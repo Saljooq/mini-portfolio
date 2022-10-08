d73dca1df09c04e42cc555ad60ce9b960410c625
Sal <altaf.saljooq@outlook.com>
2022-10-08 13:02:16 -0500

    updated to reduce caching time, also added more long-term goals for integration

--- src/routes/test/index.tsx
+++ src/routes/test/index.tsx
@@ -9,14 +9,26 @@ const Test = () => {
 
 	
 	return (
-	<div>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
+	<div class={style.maintest}>
+
+		<h1>Hello there!!</h1>
+		<h2>This is a test for Interactive content with small packet size</h2>
 
 		<p>This is a example of a <button class={style.cta} onClick={() => setVisible(!isVisible)}> simple modal</button></p>
 
+		<h3>TO BE ADDED</h3>
+		<ul>
+			<li>Some way of rendering nicely linted code</li>
+			<li>Some mechanism of adding images</li>
+			<li>Some way of displaying cards</li>
+			<li>More interactive tabs at the top</li>
+			<li>A jsonised version of input for easy update and build up for the future</li>
+		</ul>
+
+		<h2>GOAL: always keep packet size small. Preferably &lt; 50 KB (minus the image size obviously)</h2>
+		<p><a href="https://tools.pingdom.com/">Here</a> is the link to do the test</p>
+
 		<Modal isVisible={isVisible} closeModel={() => setVisible(false)} />
-		<p>Hello there!!</p>
 
 	</div>
 )};
